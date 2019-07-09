aba7b819aa8da5d715fb79079344c69c742371da
xilong.jin <xilong.jin@mirrortech.cn>
2019-07-09 18:18:19 +0800

    mybatis加入

--- src/main/java/com/klaus/base/controller/TestController.java
+++ src/main/java/com/klaus/base/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.klaus.base.controller;
 
 import com.klaus.base.entity.Result;
-import com.klaus.base.server.Controller;
 import com.klaus.base.util.ResultUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/7/8 18:16
  */
 @RestController//相当于@Controller和@ResponseBody两个注解的结合，返回json数据 但不能返回jsp，html页面，视图解析器无法解析jsp html页面
-public class TestController extends Controller {
+public class TestController{
 
     @RequestMapping(value = "/hello")
     private Result hello(){
